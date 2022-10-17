cdd3bbbaf17f5bdc75fa215e6f9047b67ca5d450
Renan <renanloub@protonmail.com>
2022-10-17 18:53:43 -0300

    adicionado o metodo post

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import db from './config/dbConnect.js';
 import livros from './models/Livro.js';
+import routes from './routes/index.js';
+
+
+
 
 db.on("error",console.log.bind(console,'Erro de conexão'))
 db.once("open", ()=>{
@@ -8,26 +12,18 @@ db.once("open", ()=>{
 })
 
 
+
+
 const app = express();
 app.use(express.json())
 
+routes(app);
 // const livros = [
 //     {id:1, "titulo":"Senhor dos Aneis"},
 //     {id:2, "titulo":"Hobbit"}
 // ]
 
 
-app.get("/", (req,res)=>{
-    res.status(200).send('Hello World')
-})
-
-app.get('/livros',(req,res)=>{
-    livros.find((err,livros)=>{
-        res.status(200).json(livros)
-    })
-
-    
-})
 app.get('/livros/:id',(req,res)=>{
     let index = buscaLivro(req.params.id)
     res.json(livros[index])
